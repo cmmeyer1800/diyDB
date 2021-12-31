20d3419ad0184469458089bf98e6f00d836332b8
Server <null@null.com>
2021-12-31 00:38:50 +0000

    Auto formatter run

--- src/db_shell/db_shell.cpp
+++ src/db_shell/db_shell.cpp
@@ -1,51 +1,46 @@
 #include "db_shell.h"
 
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 #include <vector>
 
-void DBShell::printWelcome(){
-    std::cout << attr.bold << "Welcome to the DiyDB shell\n" << attr.none << "Enter help for usage details" << std::endl;
+void DBShell::printWelcome() {
+  std::cout << attr.bold << "Welcome to the DiyDB shell\n"
+            << attr.none << "Enter help for usage details" << std::endl;
 }
 
-std::string DBShell::getInput(){
-    std::string ret;
-    std::getline(std::cin, ret);
-    return ret;
+std::string DBShell::getInput() {
+  std::string ret;
+  std::getline(std::cin, ret);
+  return ret;
 }
 
-int DBShell::parseInput(std::string input){
+int DBShell::parseInput(std::string input) {
+  std::vector<std::string> words;
+  std::istringstream ss;
+  ss.str(input.c_str());
+  for (std::string word; std::getline(ss, word, ' ');) {
+    words.push_back(word);
+  }
 
-    std::vector<std::string> words;
-    std::istringstream ss;
-    ss.str(input.c_str());
-    for(std::string word; std::getline(ss, word, ' '); ){
-        words.push_back(word);
-    }
+  if (words.size() < 1) {
+    return false;
+  }
 
-    if(words.size() < 1){
-        return false;
-    }
+  std::string first = words[0];
 
-    std::string first = words[0];
-
-    if(first == "q" || first == "quit" || first == "exit"){
-        exit(0);
+  if (first == "q" || first == "quit" || first == "exit") {
+    exit(0);
+  } else if (first == "clear") {
+    std::cout << "\033c";
+  } else if (first == "create") {
+    if (words.size() != 3) {
+      return false;
+    } else {
     }
-    else if(first == "clear"){
-        std::cout << "\033c";
-    }
-    else if(first == "create"){
-        if(words.size() != 3){
-            return false;
-        }
-        else{
-            
-        }
-    }
-    else{
-        return false;
-    }
-    return true;
+  } else {
+    return false;
+  }
+  return true;
 }
--- src/db_shell/db_shell.h
+++ src/db_shell/db_shell.h
@@ -3,31 +3,28 @@
 #include <string>
 
 class DBShell {
-
  public:
+  struct {
+    const std::string none = "\e[0m";
+    const std::string bold = "\e[1m";
+    const std::string dim = "\e[2m";
+    const std::string underline = "\e[4m";
+    const std::string blink = "\e[5m";
+    const std::string inv = "\e[7m";
+    const std::string black = "\e[30m";
+    const std::string red = "\e[31m";
+    const std::string green = "\e[32m";
+    const std::string yellow = "\e[33m";
+    const std::string blue = "\e[34m";
+    const std::string magenta = "\e[35m";
+    const std::string cyan = "\e[36m";
+    const std::string lgray = "\e[37m";
+    const std::string dgray = "\e[90m";
+  } attr;
 
-    struct{
-        const std::string none        = "\e[0m";
-        const std::string bold        = "\e[1m";
-        const std::string dim         = "\e[2m";
-        const std::string underline   = "\e[4m";
-        const std::string blink       = "\e[5m";
-        const std::string inv         = "\e[7m";
-        const std::string black       = "\e[30m";
-        const std::string red         = "\e[31m";
-        const std::string green       = "\e[32m";
-        const std::string yellow      = "\e[33m";
-        const std::string blue        = "\e[34m";
-        const std::string magenta     = "\e[35m";
-        const std::string cyan        = "\e[36m";
-        const std::string lgray       = "\e[37m";
-        const std::string dgray       = "\e[90m";
-    }attr;
+  void printWelcome();
 
-    void printWelcome();
+  std::string getInput();
 
-    std::string getInput();
-
-    int parseInput(std::string input);
-
+  int parseInput(std::string input);
 };
