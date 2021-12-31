8f8ec486c2cf9d447a8fd3095c1e431f75b17220
Collin Meyer <collinmmeyer@gmail.com>
2021-12-30 18:38:14 -0600

    work on DBShell begun

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
-#include <fstream>
 #include <iostream>
 #include <string>
 
 #include "binary_conv.hpp"
+#include "db_shell.h"
 #include "hashtable.hpp"
 #include "dict.hpp"
 #include "log.hpp"
 
 int main(int argc, char* argv[]){
-  ds::Dict<unsigned> t;
+  bool running = true;
+  std::string buffer;
+  DBShell shell;
+
+  shell.printWelcome();
   
+  while(running){
+    std::cout << "~ DBShell $ ";
+    
+    std::string input = shell.getInput();
+    int select = shell.parseInput(input);
+    if(!select){
+      std::cout << shell.attr.red << shell.attr.bold << "Error: Command Could Not Be Understood" << shell.attr.none << std::endl;
+    }
+  }
+  return 0;
 }
